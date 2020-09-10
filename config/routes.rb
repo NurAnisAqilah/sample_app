464f4120dc8d3cac9656d8cbf98136e9b1a6081e
NurAnisAqilah <anisaqilahzack@gmail.com>
2020-09-10 18:30:17 +0800

    Chapter7

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /en/ do
-    root "static_pages#home"
-    get "static_pages/help"
-    get "static_pages/contact"
-  end
+  root "static_pages#home"
+  get "users/show"
+  get "users/new"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+
+  resources :users, only: %i(show new)
 end

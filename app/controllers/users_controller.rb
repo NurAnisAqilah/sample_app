df5131d129088f6911b7dcf058e38903dba0fd6f
NurAnisAqilah <anisaqilahzack@gmail.com>
2020-09-23 18:48:38 +0800

    Chapter11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,67 +1,89 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, except: %i(show new create)
+  before_action :logged_in_user, only: %i(show new create)
+  before_action :load_user, only: %i(index new create)
   before_action :correct_user, only: %i(edit update)
-  before_action :admin_user, only: :destroy
-  before_action :load_user, except: %i(index new create)
+  before_action :admin_user, only: :destroy 
+  
+  def show
+    @user = User.find_by id: params[:id]
+  end 
+    flash[:danger] = "User not found!"
+    redirect_to root_path
 
   def index
-    @users = User.page(params[:page]).per Settings.paging
+      @users = User.page(params[:page]).per Settings.per_page
   end
 
   def new
     @user = User.new
   end
 
+  def show; end
+
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
-      flash[:danger] = "Please try again!"
+      flash[:danger] = t "user.new.danger"
       render :new
     end
-  end
-
+  end  
+  
   def update
+    @user = User.find_by id: params[:id]
     if @user.update_attributes user_params
-      flash[:success] = "Profile updated"
+      flash[:success] = t "user.update"
       redirect_to @user
     else
+      flash.now[:danger] = t "user.update_fail"
       render :edit
-      flash[:danger] = "Profile update unsuccessful"
     end
   end
 
+  def edit
+    @user = User.find_by id: params[:id]
+  end
+  
   def destroy
-    User.find_by(id: params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
+    if @user.destroy
+      flash[:success] = t "user.delete"
+    else
+      flash[:danger] = t "user.delete_fail"
+    end
+    redirect_to users_path
+  end
+
+  private
+  
+  def user_params
+    params.require:(user).permit :name, :email, :password, :password_confirmation
   end
 
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "Please log in."
+      flash[:danger] = t "user.error.login"
       redirect_to login_url
     end
   end
 
+  def load_user
+    @user  = User.find_by id: params[:id]
+    return if @user.present?
+
+    flash[:warning] = t "user.new.absent"
+    redirect_to root_path
+  end
+
   def correct_user
-    @user = User.find_by id: params[:id]
-    return if current_user?@user
-    
-    flash[:danger] = "Unauthorized entry"
-    redirect_to root_url
+    redirect_to root_path unless current_user? @user
   end
 
   def admin_user
-    redirect_to root_url unless current_user.admin?
-  end
-
-  private
-  def user_params
-    params.require(:user).permit :name, :email, :password, :password_confirmation
+    redirect_to root_url unless current_user.admin?  
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ email:
 "example@railstutorial.org",
 password: "foobar",
 password_confirmation: "foobar"
-admin: true)
+admin: true, activated: true, activated_at: Time.zone.now))
 99.times do |n|
 name = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
@@ -18,5 +18,5 @@ password = "password"
 User.create!(name: name,
 email: email,
 password: password,
-password_confirmation: password)
+password_confirmation: password, activated: true, activated_at: Time.zone.now))
 end
